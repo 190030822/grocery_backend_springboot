f89fd00f458cdae6e2d041ba301febcec2858e7d
190030822 <190030822@kluniversity.in>
2023-11-09 10:04:49 +0530

    description of end points

--- src/main/java/com/explorecode/funwithapis/controller/tryingFirstController.java
+++ src/main/java/com/explorecode/funwithapis/controller/tryingFirstController.java
@@ -8,6 +8,9 @@ public class tryingFirstController {
     
     @GetMapping("/") 
     public String firstendpoint() {
-        return "Happy to do with first endpoint. Successfully runned";
+        return "These are grocery api endpoinds. \n EndPoints are : \n -> GET PRODUCTS : /getProducts \n -> GET PRDOUCT BY NAME : /getProduct/name/{name_of_the_product} \n -> DELETE PRODUCT BY ID : deleteProduct/{product_id} \n"
+        + " -> GET PRODUCT BY ID : /getProduct/{product_id} \n "
+        + " -> CREATE PRODUCT : /addProduct \n "
+        + " -> ADD MULTIPLE PRODUCTS : /addMultipleProducts";
     }
 }
